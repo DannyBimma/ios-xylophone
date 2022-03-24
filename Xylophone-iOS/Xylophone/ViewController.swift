f10e279d8d33945a95c91987ad455a5b084fec37
Danny Bimma <82052316+DannyBimma@users.noreply.github.com>
2022-03-24 19:10:16 -0400

    Playback functionality implementation
    Code was added to have sound played back to one of the buttons.

--- Xylophone-iOS/Xylophone/ViewController.swift
+++ Xylophone-iOS/Xylophone/ViewController.swift
@@ -7,6 +7,9 @@
 //
 
 import UIKit
+import AVFoundation
+
+var player: AVAudioPlayer?
 
 class ViewController: UIViewController {
 
@@ -16,6 +19,31 @@ class ViewController: UIViewController {
 
     @IBAction func btnPressed(_ sender: UIButton) {
         print("C btn touched up inside!")
+        
+        // call playSound function
+        playSound()
+    }
+    
+    func playSound() {
+        guard let url = Bundle.main.url(forResource: "C", withExtension: "wav") else { return }
+
+        do {
+            try AVAudioSession.sharedInstance().setCategory(.playback, mode: .default)
+            try AVAudioSession.sharedInstance().setActive(true)
+
+            /* The following line is required for the player to work on iOS 11. Change the file type accordingly*/
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileType.wav.rawValue)
+
+            /* iOS 10 and earlier require the following line:
+            player = try AVAudioPlayer(contentsOf: url, fileTypeHint: AVFileTypeMPEGLayer3) */
+
+            guard let player = player else { return }
+
+            player.play()
+
+        } catch let error {
+            print(error.localizedDescription)
+        }
     }
     
     
