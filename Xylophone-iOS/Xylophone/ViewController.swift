19154232642d7a5880a2964b58eaf1623d1caae8
Danny Bimma <82052316+DannyBimma@users.noreply.github.com>
2022-03-24 17:57:11 -0400

    Init Commit
    Starting files

---
+++ Xylophone-iOS/Xylophone/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  Xylophone
+//
+//  Created by Angela Yu on 28/06/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+    @IBAction func btnPressed(_ sender: UIButton) {
+        print("C btn touched up inside!")
+    }
+    
+    
+
+}
+
